380a10baa97b4d9a07d51ff4c6109bedec41c847
Mustafa Jebara <moosejebara@gmail.com>
2016-02-27 12:00:50 -0500

    Added files via upload

--- Proj02/Employee.h
+++ Proj02/Employee.h
@@ -1,4 +1,3 @@
-
 #ifndef CEMPLOYEE_H
 #define CEMPLOYEE_H
 
@@ -9,38 +8,68 @@ using namespace std;
 
 class CEmployee {
 protected:
-    string mFirstName;
-    string mLastName;
-    unsigned int mSalary;
-    tm mHiringYear;
-    
+	string mFirstName;
+	string mLastName;
+	unsigned int mSalary;
+	tm mHiringYear;
+
 public:
-    CEmployee(string firstName, string lastName, unsigned int salary, tm yearHired):
-    mFirstName(firstName),mLastName(lastName),mSalary(salary),mHiringYear(yearHired){};
-    
-    CEmployee(const CEmployee& CE) {
-    mFirstName=CE.mFirstName;
-    mLastName=CE.mLastName;
-    mSalary=CE.mSalary;
-    mHiringYear=CE.mHiringYear;
-}
+	CEmployee(string firstName, string lastName, unsigned int salary, tm yearHired) :
+		mFirstName(firstName), mLastName(lastName), mSalary(salary), mHiringYear(yearHired) {};
 
-CEmployee& operator = (const CEmployee& CE){
-    if(this!=&CE){
-        mFirstName=CE.mFirstName;
-        mLastName=CE.mLastName;
-        mSalary=CE.mSalary;
-        mHiringYear=CE.mHiringYear;
-    }
-    return *this;
-}
+	CEmployee(const CEmployee& CE) {
+		mFirstName = CE.mFirstName;
+		mLastName = CE.mLastName;
+		mSalary = CE.mSalary;
+		mHiringYear = CE.mHiringYear;
+	}
 
-~CEmployee() {}
+	CEmployee& operator = (const CEmployee& CE) {
+		if (this != &CE) {
+			mFirstName = CE.mFirstName;
+			mLastName = CE.mLastName;
+			mSalary = CE.mSalary;
+			mHiringYear = CE.mHiringYear;
+		}
+		return *this;
+	}
 
-virtual void DisplayEmployee(){
-    cout<<mFirstName<<" "<<mLastName<<"\t Salary:"<<mSalary<<"\t Hiring Year:"<<mHiringYear.tm_year;
-}
+	~CEmployee() {}
+
+	virtual void DisplayEmployee() {
+		cout << mFirstName << " " << mLastName << "\t Salary:" << mSalary << "\t Hiring Year:" << mHiringYear.tm_year;
+	}
+	string getFirstName() const {
+		return mFirstName;
+	}
+
+	string getLastName() const {
+		return mLastName;
+	}
+
+	tm getYearHired() const {
+		return mHiringYear;
+	}
+
+	unsigned short getSalary() const {
+		return mSalary;
+	}
+
+	void setFirstName(const string firstName) {
+		mFirstName = firstName;
+	}
+
+	void setLastName(const string lastName) {
+		mLastName = lastName;
+	}
+
+	void setYearHired(const tm yearHired) {
+		mHiringYear = yearHired;
+	}
+
+	void setSalary(const unsigned short salary) {
+		mSalary = salary;
+	}
 };
 
 #endif /* CEMPLOYEE_H */
-
