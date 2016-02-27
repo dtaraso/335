36173cdccdcdb403fd32f45909bfa3722182ecd8
Mustafa Jebara <moosejebara@gmail.com>
2016-02-27 12:06:53 -0500

    Update EmployeeDatabaseSortbyLastName.h

--- Proj02/EmployeeDatabaseSortbyLastName.h
+++ Proj02/EmployeeDatabaseSortbyLastName.h
@@ -1,27 +1,26 @@
-/* 
- * File:   EmployeeDatabaseSortbyLastName.h
- * Author: Dasha
- *
- * Created on February 26, 2016, 4:56 PM
- */
+/*
+* File:   EmployeeDatabaseSortbyLastName.h
+* Author: Dasha
+*
+* Created on February 26, 2016, 4:56 PM
+*/
 
 #ifndef EMPLOYEEDATABASESORTBYLASTNAME_H
 #define	EMPLOYEEDATABASESORTBYLASTNAME_H
 #include "EmployeeDatabaseSortable.h"
 
-class EmployeeDatabaseSortbyLastName: public EmployeeDatabaseSortable {
-    public: 
-        EmployeeDatabaseSortbyLastName(vector <Student*> studentVector):
-    StudentVector(studentVector){};
-    
-        virtual bool smaller(int i, int j) const {
-        if (getStudent(i)->getFirstName() < getStudent(j)->getFirstName())
-            return true;
-        else
-            return false;
-    }
+class EmployeeDatabaseSortByLastName : public EmployeeDatabaseSortable {
+public:
+	EmployeeDatabaseSortByLastName(vector <CEmployee*> employees) :
+		EmployeeDatabaseSortable(employees) {};
+
+	virtual bool smaller(int i, int j) const {
+		if (getEmployee(i)->getFirstName() < getEmployee(j)->getFirstName())
+			return true;
+		else
+			return false;
+	}
 };
 
 
 #endif	/* EMPLOYEEDATABASESORTBYLASTNAME_H */
-
