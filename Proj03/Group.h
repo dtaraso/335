9c1546dc044e7c739921ab7f8cccd7767d43bc1f
Mustafa Jebara <moosejebara@gmail.com>
2016-03-21 20:53:45 -0400

    Update Group.h

--- Proj03/Group.h
+++ Proj03/Group.h
@@ -1,14 +1,68 @@
-/* 
- * File:   Group.h
- * Author: Dasha
- *
- * Created on March 20, 2016, 5:01 PM
- */
+/*
+* File:   Group.h
+* Author: Dasha
+*
+* Created on March 20, 2016, 5:01 PM
+*/
 
 #ifndef GROUP_H
 #define	GROUP_H
+#include "Unit.h"
+#include <string>
+#include <vector>
+#include "Employee.h"
+
+class Group: public Unit {
+public:
+	string mGroupName;
+	vector<Employee*> mEmployees;
+
+	//constructor
+	Group(string groupname) : mGroupName(groupname) {};
+
+	//copy constructor
+	Group(const Group& group) {
+		mGroupName = group.mGroupName;
+		mEmployees = group.mEmployees;
+	}
+
+	//Assignment Operator
+	Group& operator = (const Group& group) {
+		if (this != &group) {
+			mGroupName = group.mGroupName;
+			mEmployees = group.mEmployees;
+		}
+		return *this;
+	}
+
+	//destructor
+	~Group() {}
+
+	string getGroupName() {
+		return mGroupName;
+	}
+
+	string setGroupName(const string& gname) {
+		mGroupName = gname;
+	}
+
+	void addGroupMember(Employee* group) {
+		mEmployees.push_back(group);
+	}
+
+	virtual void print() {
+		cout << "Group Name: "<< mGroupName << endl;
+		for (auto member : mEmployees) {
+			member->print();
+		}
+
+	}
+
+	virtual void Accept(Visitor* v) {
+		cout << endl;
+	}
+};
 
 
 
 #endif	/* GROUP_H */
-
