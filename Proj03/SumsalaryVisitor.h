e4933fb63750b1360142250a0828c5def24d7337
Mustafa Jebara <moosejebara@gmail.com>
2016-03-22 10:57:04 -0400

    Update SumsalaryVisitor.h

--- Proj03/SumsalaryVisitor.h
+++ Proj03/SumsalaryVisitor.h
@@ -1,29 +1,44 @@
-/* 
- * File:   SumsalaryVisitor.h
- * Author: Dasha
- *
- * Created on March 20, 2016, 5:01 PM
- */
+/*
+* File:   SumsalaryVisitor.h
+* Author: Dasha
+*
+* Created on March 20, 2016, 5:01 PM
+*/
 
 #ifndef SUMSALARYVISITOR_H
 #define	SUMSALARYVISITOR_H
 
 #include "Employee.h"
+#include "Visitor.h"
 
 class SumsalaryVisitor: public Visitor {
 public:
-    unsigned int mSum;
+	unsigned int mSum;
 public:
-    double getTotalSalary() {return mSum;}
-    
-    virtual void VisitEmployee(Employee* emp){
-        mSum += (emp->getSalary());
-    }
-    
-    double restTotalSalary() {
-        mSum = 0;
-    }
+	double getTotalSalary() { return mSum; }
+
+	virtual void VisitEmployee(Employee* emp) {
+		mSum += (emp->getSalary());
+	}
+
+	double restTotalSalary() {
+		mSum = 0;
+	}
+
+	/*
+	virtual void VisitEmployee(Employee* emp) {
+
+	}
+	virtual void VisitManager(Manager* man) {
+
+	}
+	virtual void VisitGroup(Group* grp) {
+
+	}
+	virtual void VisitDepartment(Department* dep) {
+
+	}
+	*/
 };
 
 #endif	/* SUMSALARYVISITOR_H */
-
