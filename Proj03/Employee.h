0c91bb45aa06eaf8d5b9427185fbb5cacd9baea3
Mustafa Jebara <moosejebara@gmail.com>
2016-03-21 20:53:10 -0400

    Update Employee.h

--- Proj03/Employee.h
+++ Proj03/Employee.h
@@ -4,93 +4,96 @@
 #include <string>
 #include <ctime>
 #include <iostream>
+#include "Unit.h"
 using namespace std;
 
-class Employee {
+class Employee : public Unit {
 protected:
-    //Initializes employee elements
+	//Initializes employee elements
 	string mFirstName;
 	string mLastName;
 	unsigned int mSalary;
-	tm mHiringYear;
-        unsigned int mID;
+	int mHiringYear;
+	unsigned int mID;
 
 public:
-    //Constructor
-	Employee(string firstName, string lastName, unsigned int salary, tm yearHired, unsigned int ID) :
+	//Constructor
+	Employee(string firstName, string lastName, unsigned int salary, int yearHired, unsigned int ID) :
 		mFirstName(firstName), mLastName(lastName), mSalary(salary), mHiringYear(yearHired), mID(ID) {};
 
-    //Copy Constructor
+	//Copy Constructor
 	Employee(const Employee& CE) {
 		mFirstName = CE.mFirstName;
 		mLastName = CE.mLastName;
 		mSalary = CE.mSalary;
 		mHiringYear = CE.mHiringYear;
-                mID = CE.mID;
+		mID = CE.mID;
 	}
 
-    //Assignment Operator
+	//Assignment Operator
 	Employee& operator = (const Employee& CE) {
 		if (this != &CE) {
 			mFirstName = CE.mFirstName;
 			mLastName = CE.mLastName;
 			mSalary = CE.mSalary;
 			mHiringYear = CE.mHiringYear;
-                        mID = CE.mID;
+			mID = CE.mID;
 		}
 		return *this;
 	}
 
-    //Destructor
+	//Destructor
 	~Employee() {}
 
-    //Prints elements of employees
-	virtual void DisplayEmployee() {
-		cout << mFirstName << " " << mLastName << "\t Salary:" << mSalary << "\t Hiring Year:" << mHiringYear.tm_year << "\t ID:" << mID;
-	}
-    //returns first name
+	//returns first name
 	string getFirstName() const {
 		return mFirstName;
 	}
-    //returns last name
+	//returns last name
 	string getLastName() const {
 		return mLastName;
 	}
-    //returns year hired
-	tm getYearHired() const {
+	//returns year hired
+	int getYearHired() const {
 		return mHiringYear;
 	}
-        
-    //returns salary
+
+	//returns salary
 	unsigned short getSalary() const {
 		return mSalary;
 	}
-        
-    //returns ID
-        unsigned int getID() const {
-            return mID;
-        }
-        
-    //sets first name
+
+	//returns ID
+	unsigned int getID() const {
+		return mID;
+	}
+
+	//sets first name
 	void setFirstName(const string firstName) {
 		mFirstName = firstName;
 	}
-    //sets last name
+	//sets last name
 	void setLastName(const string lastName) {
 		mLastName = lastName;
 	}
-    //sets year hired
-	void setYearHired(const tm yearHired) {
+	//sets year hired
+	void setYearHired(const int yearHired) {
 		mHiringYear = yearHired;
 	}
-    //sets salary
+	//sets salary
 	void setSalary(const unsigned short salary) {
 		mSalary = salary;
 	}
-        
-        void setID(const unsigned int ID) {
-            mID = ID;
-        }
+
+	void setID(const unsigned int ID) {
+		mID = ID;
+	}
+
+	virtual void print() {
+		cout << "\t\t" << mFirstName << " " << mLastName << "\t Salary:" << mSalary << "\t Hiring Year:" << mHiringYear << "\t ID:" << mID;
+	};
+
+	virtual void Accept(Visitor*v) {};
 };
 
 #endif /* EMPLOYEE_H */
